5fa96e4eb806aa42f8bdf44f25766385ae6c75f3
Safayet <mrishafayet003@gmail.com>
2022-03-21 12:53:45 +0600

    add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Menubar from './Components/Menubar/Menubar';
-import AllFoducts from './Components/AllFoducts/AllFoducts';
-import { useEffect, useState } from 'react';
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Navigation from './components/Navigation/Navigation';
+import Blogs from './components/Blogs/Blogs';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import ProjectDetail from './components/ProjectDetail/ProjectDetail';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
-  const [Count, setCount] = useState(0);
-  const setCardCount = () => {
-    setCount(Count + 1);
-  }
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
   return (
-    <div className="App">
-      <Menubar Count={Count}></Menubar>
-      <AllFoducts setCardCount={setCardCount}></AllFoducts>
-
+    <div>
+      <BrowserRouter>
+      <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/projectDetail/:id" element={<ProjectDetail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 export default App;
